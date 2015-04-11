670f05bef5280c9e4898e179cc2266a0baeec196
melindae <melindae@gmail.com>
2015-04-11 20:58:25 +0000

    set css, firebase configured/synced with array

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,56 +1,29 @@
-var toDo = angular.module('ToDo', ['ui.router', 'firebase']);
+var doThings = angular.module('doThings', ['ui.router', 'firebase']);
 
+doThings.controller('doController', function($scope, $firebaseArray) {
+  var fireRef = new Firebase("https://blinding-fire-8984.firebaseio.com/tasks");
 
-mouse hover = time since entry
+    $scope.tasks = $firebaseArray(fireRef);
 
-mouse click = edit 
+  $scope.addtask = function() {
+    $scope.tasks.$add( {
+        'timein': $scope.timein,
+        'name': $scope.name,
+        'value': $scope.value,
+        'done': $scope.done,
 
-var taskTemplate =  {
+    });  
 
-  *task: 'do something',
-  *priority: '4 options',
-  *time-stamp: 'enter time',
-  *taskComplete: 'y/n',
-  
-  // taskCompleteStamp 'some date',
-  // type: 'self populating',
-  // deadline: 'day or days',
-  // actionDay: 'specific day',
-  // repeat: 'y, n, week, plus',
-  // extra notes: 'blah blah',
-
-}
-
-
-
-
-
-  var albumPicasso = {
-   name: 'The Colors',
-   artist: 'Pablo Picasso',
-   label: 'Cubism',
-   year: '1881',
-   albumArtUrl: '/images/album-placeholder.png',
-
-   songs: [
-       { name: 'Blue', length: '4:26' },
-       { name: 'Green', length: '3:14' },
-       { name: 'Red', length: '5:01' },
-       { name: 'Pink', length: '3:21'},
-       { name: 'Magenta', length: '2:15'}
-     ]
   };
 
-  <div ng-repeat="album in albums" class="collection-album-container col-md-2">
-    <div class="collection-album-info caption">
-      <p>
-        <a class="album-name" ui-sref="album"> {{ album.name }} </a>
-          <br/>
-        <a ui-sref="album"> {{ album.artist }} </a>
-          <br/>
-           {{ album.songs.length }} songs
-          <br/>
-      </p>
-    </div>
-  </div>
+});
 
+// Firebase.ServerValue.TIMESTAMP
+// To get the actual server time from Firebase for a write of some kind, 
+// you would first write to Firebase 
+// (i.e. ref.child('updated_at').set(Firebase.ServerValue.TIMESTAMP); 
+// and have listeners for that data 
+// (ref.on('value', function(snapshot) { // snapshot.val().updated_at });). 
+// There is no way to get the server time synchronously from Firebase, 
+// but the local time + server offset time (provided immediately whenever 
+// you write to Firebase using the ServerValue) is a good approximation.
