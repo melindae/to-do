71e4c4988953966851006439032e0f25cca35037
melindae <melindae@gmail.com>
2015-11-02 19:20:02 +0000

    edited readme.md

--- README.md
+++ README.md
@@ -1,35 +1,26 @@
 git init
-
 npm install -g grunt-cli
-
 sudo chown -R $USER /usr/local
-
 npm install
 
+*** Git ***
 
-
-
-git config user.name "melindae"
-
-git config user.email "melindae@gmail.com"
+git config user.name "yourName"
+git config user.email "you@email.com"
 
 add and commit
 
-git remote add origin https://github.com/melindae/yourProjectName.git
+git remote add origin https://github.com/yourName/yourProjectName.git
 git push -u origin master
 
 
+*** Heroku ***
 
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 git remote add heroku git@heroku.com:your-app.git
  -or-
-heroku git:remote -a shrouded-oasis-4528
-
-
-
-
+heroku git:remote -a yourHerokuSite
 
 git push heroku master
-
 * git push -f heroku master // if necessary
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,25 +1,21 @@
 var doThings = angular.module('doThings', ['ui.router', 'firebase']);
 
-doThings.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
-
-  $stateProvider
-    .state('home', {
+doThings.config(['$stateProvider', '$locationProvider',
+  function($stateProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $stateProvider.state('home', {
       url: '',
       controller: 'doController',
       templateUrl: 'templates/home.html'
-    })
-    .state('old', {
+    }).state('old', {
       url: '/oldtask',
       controller: 'oldController',
       templateUrl: 'templates/oldtask.html',
-    })
-    .state('otherwise', {
+    }).state('otherwise', {
       url: '*path',
       controller: 'doController',
       templateUrl: 'templates/home.html'
     });
-
 }]);
 
 doThings.controller('doController', function($scope, $firebaseArray, $firebaseObject) {
@@ -30,62 +26,43 @@ doThings.controller('doController', function($scope, $firebaseArray, $firebaseOb
   $scope.tasks = $firebaseArray(fireRef);
   $scope.colors = $firebaseObject(fireCol);
   var cssNow = $firebaseObject(fireCss);
-
-
   //  retrieve and apply CSS preference
   cssNow.$bindTo($scope, "data").then(function() {
-    currentCss = $scope.data.$value
-    console.log($scope.data.$value); 
-    console.log(currentCss);
-
+    var currentCss = $scope.data.$value;
     var oldlink = document.getElementsByTagName("link").item(0);
     var newlink = document.createElement("link");
-
     newlink.setAttribute("rel", "stylesheet");
     newlink.setAttribute("type", "text/css");
     newlink.setAttribute("href", currentCss);
-
     document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
   });
-
   //  change and save CSS preference
   $scope.changeCSS = function(cssFile, cssLinkIndex) {
-
     var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
     var newlink = document.createElement("link");
-
     newlink.setAttribute("rel", "stylesheet");
     newlink.setAttribute("type", "text/css");
     newlink.setAttribute("href", cssFile);
-
-    $scope.colors.css = cssFile
+    $scope.colors.css = cssFile;
     $scope.colors.$save().then(function(ref) {
       ref.key() === $scope.colors.$id; // true
     }, function(error) {
       console.log("Error:", error);
     });
-
     document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
-  }
-
-
-
+  };
   $scope.thing = {
     name: ""
   };
   $scope.taskSee = 'All';
-
-  // document.addEventListener( 
-  //   'touchstart', 
+  // document.addEventListener(
+  //   'touchstart',
   //    function( event ){
   //     console.log('target', event.target);
   //     console.log( event );
   //   }
   // );
- 
-
   $scope.addtask = function() {
-
     $scope.tasks.$add({
       'timein': fireTime,
       'name': $scope.thing.name,
@@ -93,45 +70,28 @@ doThings.controller('doController', function($scope, $firebaseArray, $firebaseOb
       'priority': $scope.priority,
       'done': "No", //'done' defaults to 'No' here
     });
-
     $scope.thing.name = "";
   };
-
   $scope.showtask = function(taskTime, taskDone, taskType) {
-    var oneWeekAgo = new Date()
-      .getTime() - 1000 * 60 * 60 * 24 * 7, //one week
-      oneDayAgo = new Date()
-      .getTime() - 1000 * 60 * 60 * 24, //one day - for testing
-      oneHourAgo = new Date()
-      .getTime() - 1000 * 60 * 60; //one hour - for testing
-      oneYearAgo = new Date()
-      .getTime() - 1000 * 60 * 60 * 24 * 365; //one year - for demo app so tasks visible up for viewing
-
-    if ((oneYearAgo < taskTime) 
-      && (taskDone !== 'Yes!') 
-      && ((taskType == $scope.taskSee) || ($scope.taskSee == 'All')))
-      return true
+    var oneWeekAgo = new Date().getTime() - 1000 * 60 * 60 * 24 * 7, //one week
+      oneDayAgo = new Date().getTime() - 1000 * 60 * 60 * 24, //one day - for testing
+      oneHourAgo = new Date().getTime() - 1000 * 60 * 60; //one hour - for testing
+    oneYearAgo = new Date().getTime() - 1000 * 60 * 60 * 24 * 365; //one year - for demo app so tasks visible up for viewing
+    if ((oneYearAgo < taskTime) && (taskDone !== 'Yes!') && ((taskType == $scope.taskSee) || ($scope.taskSee == 'All'))) return true;
   };
 });
 
 doThings.controller('oldController', function($scope, $firebaseArray) {
   var fireRef = new Firebase("https://blinding-fire-8984.firebaseio.com/work/tasks");
   var fireTime = Firebase.ServerValue.TIMESTAMP;
-
   $scope.tasks = $firebaseArray(fireRef);
   $scope.taskSee = 'All';
-
   $scope.showtask = function(taskTime, taskDone, taskType) {
-    var oneWeekAgo = new Date()
-      .getTime() - 1000 * 60 * 60 * 24 * 7,
-      oneDayAgo = new Date()
-      .getTime() - 1000 * 60 * 60 * 24, //for testing
-      oneHourAgo = new Date()
-      .getTime() - 1000 * 60 * 60; //for testing
-
-    if (((oneWeekAgo > taskTime) || (taskDone == 'Yes!'))
-      && ((taskType == $scope.taskSee) || ($scope.taskSee == 'All')))
-      return true
+    var oneWeekAgo = new Date().getTime() - 1000 * 60 * 60 * 24 * 7,
+      oneDayAgo = new Date().getTime() - 1000 * 60 * 60 * 24, //for testing
+      oneHourAgo = new Date().getTime() - 1000 * 60 * 60; //for testing
+    oneYearAgo = new Date().getTime() - 1000 * 60 * 60 * 24 * 365; //one year - for demo app so tasks visible up for viewing
+    if (((oneYearAgo > taskTime) || (taskDone == 'Yes!')) && ((taskType == $scope.taskSee) || ($scope.taskSee == 'All'))) return true;
   };
 });
 
@@ -139,18 +99,16 @@ doThings.directive('taskComplete', function() {
   return {
     restrict: 'A',
     template: "<button class='tableDone fa fa-check-square-o' ng-click='taskDone(task)'></button>",
-
     link: function($scope, element, attrs) {
       $scope.taskDone = function(task) {
         var taskIndex = $scope.tasks.indexOf(task);
         $scope.tasks[taskIndex].done = "Yes!";
-        $scope.tasks.$save(taskIndex)
-          .then(function(fireRef) {
-            fireRef.key() === $scope.tasks[taskIndex].$id;
-          });
-      }
+        $scope.tasks.$save(taskIndex).then(function(fireRef) {
+          fireRef.key() === $scope.tasks[taskIndex].$id;
+        });
+      };
     }
-  }
+  };
 });
 
 doThings.directive('taskDelete', function() {
@@ -160,11 +118,10 @@ doThings.directive('taskDelete', function() {
     link: function($scope, element, attrs) {
       $scope.taskDelete = function(task) {
         var taskIndex = $scope.tasks.indexOf(task);
-        $scope.tasks.$remove(taskIndex)
-          .then(function(fireRef) {
-            fireRef.key() === $scope.tasks[taskIndex].$id;
-          });
-      }
+        $scope.tasks.$remove(taskIndex).then(function(fireRef) {
+          fireRef.key() === $scope.tasks[taskIndex].$id;
+        });
+      };
     }
-  }
-}); 
+  };
+});
